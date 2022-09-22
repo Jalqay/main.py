95a9de576ed4e1d4bb58107d1f67ea043cf4d91a
Jalqay <ilyas.serikckanov2017@yandex.ru>
2022-09-22 18:57:24 +0600

    Первый экран

--- Kursovoi/main.py
+++ Kursovoi/main.py
@@ -1,81 +1,53 @@
 from kivy.lang import Builder
 
 from kivymd.app import MDApp
+from kivymd.uix.button import MDRaisedButton
 
 KV = '''
-MDScreenManager:
+MDScreen:
 
-    MDScreen:
-        name: "screen A"
-        md_bg_color: "lightblue"
-
-        MDHeroFrom:
-            id: hero_from
-            tag: "hero"
-            size_hint: None, None
-            size: "120dp", "120dp"
-            pos_hint: {"top": .98}
-            x: 24
-
-            FitImage:
-                source: "https://github.com/kivymd/internal/raw/main/logo/kivymd_logo_blue.png"
-                size_hint: None, None
-                size: hero_from.size
-
-        MDRaisedButton:
-            text: "Move Hero To Screen B"
-            pos_hint: {"center_x": .5}
-            y: "36dp"
-            on_release:
-                root.current_hero = "hero"
-                root.current = "screen B"
-
-    MDScreen:
-        name: "screen B"
-        hero_to: hero_to
-        md_bg_color: "cadetblue"
-
-        MDHeroTo:
-            id: hero_to
-            size_hint: None, None
-            size: "220dp", "220dp"
-            pos_hint: {"center_x": .5, "center_y": .5}
-
-        MDRaisedButton:
-            text: "Go To Screen C"
-            pos_hint: {"center_x": .5}
-            y: "52dp"
-            on_release:
-                root.current_hero = ""
-                root.current = "screen C"
-
-        MDRaisedButton:
-            text: "Move Hero To Screen A"
-            pos_hint: {"center_x": .5}
-            y: "8dp"
-            on_release:
-                root.current_hero = "hero"
-                root.current = "screen A"
-
-    MDScreen:
-        name: "screen C"
-
+    BoxLayout:
+        orientation: "vertical"
+        
+        MDTopAppBar:
+            title: "Экономика сабағы"
+            md_bg_color: .35,.11,.40,1
+            elevation: 20
+            
+            
+            
         MDLabel:
-            text: "Screen C"
-            halign: "center"
+            
 
         MDRaisedButton:
-            text: "Back To Screen B"
-            pos_hint: {"center_x": .5}
-            y: "36dp"
-            on_release:
-                root.current = "screen B"
+            text: "Уроки"
+            md_bg_color: "red"
+            md_bg_color: .24,.22,.40, 1
+            pos_hint: {"center_x": .5, "center_y": .9}
+            elevation: 20
+            font_size: "30sp"
+            
+        MDLabel:
+            
+        MDRaisedButton:
+            text: "Тесты"
+            md_bg_color: .24,.22,.40, 1
+            pos_hint: {"center_x": .5, "center_y": .9}
+            elevation: 20
+            font_size: "30sp"
+        
+        MDLabel:
+        MDLabel:
+        
 '''
 
 
 class Test(MDApp):
     def build(self):
-        return Builder.load_string(KV)
+        screen = Builder.load_string(KV)
+
+        return screen
+
 
 
 Test().run()
